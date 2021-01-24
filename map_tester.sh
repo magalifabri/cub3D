28f3f1686191fe360a91fcefcf1f51fbeffcb4e2
Magali Fabri <mfabri@mi-r4-p3.s19.be>
2021-01-24 11:09:30 +0100

    update

--- map_tester.sh
+++ map_tester.sh
@@ -87,19 +87,19 @@ printf "executing map: error_unknown_symbol.cub\n"
 cat leaks.txt | grep '0 leaks'
 
 printf '\n'
+printf "executing map: error_duplicate_colours.cub\n"
+./cub3D maps/test_cubs/error_duplicate_colours.cub
+cat leaks.txt | grep '0 leaks'
+
+printf '\n'
+printf "executing map: error_duplicate_resolution.cub\n"
+./cub3D maps/test_cubs/error_duplicate_resolution.cub
+cat leaks.txt | grep '0 leaks'
+
+printf '\n'
 
 printf "\033[1;32mTESTING MAPS THAT SHOULD PASS\033[0m\n\n"
 
-printf "executing map: pass_duplicate_colours.cub\n"
-./cub3D maps/test_cubs/pass_duplicate_colours.cub
-cat leaks.txt | grep '0 leaks'
-
-printf '\n'
-printf "executing map: pass_duplicate_resolution.cub\n"
-./cub3D maps/test_cubs/pass_duplicate_resolution.cub
-cat leaks.txt | grep '0 leaks'
-
-printf '\n'
 printf "executing map: pass_res_high.cub\n"
 ./cub3D maps/test_cubs/pass_res_high.cub
 cat leaks.txt | grep '0 leaks'
