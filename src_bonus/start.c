5b43ee1dfe17b0b72f422a43b6365087848796d0
Magali Fabri <magali@Magalis-MacBook-Pro.local>
2021-01-27 21:13:26 +0100

    fix: segfault cause in get_pxl_for_bmp

--- src_bonus/start.c
+++ src_bonus/start.c
@@ -6,7 +6,7 @@
 /*   By: mfabri <mfabri@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 11:02:40 by mfabri            #+#    #+#             */
-/*   Updated: 2021/01/27 18:30:22 by mfabri           ###   ########.fr       */
+/*   Updated: 2021/01/27 21:06:16 by mfabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -112,17 +112,20 @@ int			main(int ac, char **av)
 	initialise_variables(&t);
 	parse_cub_file(&t, ac, av);
 	t.mlx = mlx_init();
-	t.win = mlx_new_window(t.mlx, t.win_w, t.win_h, "cub3D");
 	t.img = mlx_new_image(t.mlx, t.win_w, t.win_h);
 	t.addr = mlx_get_data_addr(t.img, &t.bpp, &t.line_len, &t.endian);
-	mlx_mouse_hide();
-	mlx_mouse_move(t.win, t.win_w / 2, t.win_h / 2);
-	t.prev_mouse_x = t.win_w / 2;
 	get_textures(&t);
-	mlx_hook(t.win, 2, 1L << 0, keypress_hook, &t);
-	mlx_hook(t.win, 3, 1L << 1, keyrelease_hook, &t);
-	mlx_hook(t.win, 6, 1L << 6, mouse_move_hook, &t);
-	mlx_hook(t.win, 17, 0, exit_cub3d, &t);
+	if (!t.save)
+	{
+		t.win = mlx_new_window(t.mlx, t.win_w, t.win_h, "cub3D");
+		mlx_mouse_hide();
+		mlx_mouse_move(t.win, t.win_w / 2, t.win_h / 2);
+		t.prev_mouse_x = t.win_w / 2;
+		mlx_hook(t.win, 2, 1L << 0, keypress_hook, &t);
+		mlx_hook(t.win, 3, 1L << 1, keyrelease_hook, &t);
+		mlx_hook(t.win, 6, 1L << 6, mouse_move_hook, &t);
+		mlx_hook(t.win, 17, 0, exit_cub3d, &t);
+	}
 	mlx_loop_hook(t.mlx, run_game, &t);
 	mlx_loop(t.mlx);
 	return (0);
